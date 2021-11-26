54e71153a506249d6f51d1ccf65369a47774a1ff
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2021-11-26 17:50:05 +0300

    Build: Fix conflict of JDK_16 and JDK_16_0/JDK_17_0 env names

--- buildSrc/src/main/kotlin/tasks.kt
+++ buildSrc/src/main/kotlin/tasks.kt
@@ -229,7 +229,7 @@ fun Project.projectTest(
 
         JdkMajorVersion.values().forEach { version ->
             project.getToolchainLauncherFor(version).orNull?.let {
-                environment(version.name, it.metadata.installationPath.asFile.absolutePath)
+                environment(version.envName, it.metadata.installationPath.asFile.absolutePath)
             }
         }
     }.apply { configure(body) }
